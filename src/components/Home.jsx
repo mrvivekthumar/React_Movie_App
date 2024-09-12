2f75a2dcfa6c4f5261940544b9c8c00bae984c99
mrvivekthumar <mrvivekthumar@gmail.com>
2024-09-13 00:50:46 +0530

    completed with filter and toatl 3 api call done for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,14 @@ import Topnav from './partials/Topnav'
 import axios from '../utils/axios'
 import { Header } from './partials/Header'
 import HorizontalCards from './partials/HorizontalCards'
+import Dropdown from './partials/Dropdown'
 
 const Home = () => {
     document.title = "Home Page"
 
     const [wallpaper, setWallpaper] = useState(null);
     const [trending, setTrending] = useState(null);
+    const [category, setCategory] = useState("all");
 
 
     const GetHeaderWallpaper = async () => {
@@ -26,7 +28,7 @@ const Home = () => {
 
     const GetTrending = async () => {
         try {
-            const { data } = await axios.get(`/trending/all/day`);
+            const { data } = await axios.get(`/trending/${category}/day`);
             setTrending(data.results);
         } catch (error) {
             console.log("Error : ", error);
@@ -34,9 +36,9 @@ const Home = () => {
     }
 
     useEffect(() => {
+        GetTrending();
         !wallpaper && GetHeaderWallpaper();
-        !trending && GetTrending();
-    }, []);
+    }, [category]);
 
 
 
@@ -46,6 +48,12 @@ const Home = () => {
             <div className='w-[80%] h-full overflow-auto overflow-x-hidden'>
                 <Topnav />
                 <Header data={wallpaper} />
+
+                <div className='flex justify-between p-5'>
+                    <h1 className='text-3xl font-semibold text-zinc-400 '>Trending </h1>
+                    <Dropdown title="Fiter" options={["tv", "movie", "all"]} func={(e) => setCategory(e.target.value)} />
+                </div>
+
                 <HorizontalCards data={trending} />
             </div>
         </>
--- src/components/partials/HorizontalCards.jsx
+++ src/components/partials/HorizontalCards.jsx
@@ -1,39 +1,32 @@
 import React from 'react'
+import Dropdown from './Dropdown'
 
 const HorizontalCards = ({ data }) => {
     return (
-        <div className='w-full p-5'>
-            <div className='mb-5'>
-                <h1 className='text-3xl font-semibold text-zinc-400 '>Trending </h1>
-            </div>
-
-
-            <div className='w-[100%] flex overflow-y-hidden'>
-                {
-                    data.map((d, i) => (
-                        <div key={i} className='min-w-[15%] bg-zinc-900 mr-5 mb-5 overflow-y-auto'>
-                            <img
-                                className='w-full h-[55%] object-cover'
-                                src={`https://image.tmdb.org/t/p/original/${d.backdrop_path || d.poster_path}`}
-                                alt=""
-                            />
-                            <div className='text-white p-3 h-[45%]'>
-                                <h1 className='text-lg font-semibold'>
-                                    {d.name || d.title || d.original_name || d.original_title}
-                                </h1>
-                                <p className='text-sm'>
-                                    {d.overview.slice(0, 50)}...
-                                    <span className='text-zinc-500'>more</span>
-                                </p>
-                            </div>
-
+        <div className='w-[100%] flex overflow-y-hidden mb-5'>
+            {
+                data.map((d, i) => (
+                    <div key={i} className='min-w-[15%] bg-zinc-900 mr-5 mb-5 overflow-y-auto'>
+                        <img
+                            className='w-full h-[55%] object-cover'
+                            src={`https://image.tmdb.org/t/p/original/${d.backdrop_path || d.poster_path}`}
+                            alt=""
+                        />
+                        <div className='text-white p-3 h-[45%]'>
+                            <h1 className='text-lg font-semibold'>
+                                {d.name || d.title || d.original_name || d.original_title}
+                            </h1>
+                            <p className='text-sm'>
+                                {d.overview.slice(0, 50)}...
+                                <span className='text-zinc-500'>more</span>
+                            </p>
                         </div>
-                    ))
-                }
 
-            </div>
+                    </div>
+                ))
+            }
+
         </div>
-
     )
 }
 
